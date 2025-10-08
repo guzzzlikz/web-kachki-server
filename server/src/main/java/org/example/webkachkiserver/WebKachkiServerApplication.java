d3b86a549eb561871ab34d1d0a48b4826d7a814a
guzzzlikz <jaroslavvolk9@gmail.com>
2025-10-08 16:56:08 +0300

    Структура

---
+++ server/src/main/java/org/example/webkachkiserver/WebKachkiServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.webkachkiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebKachkiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebKachkiServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/org/example/webkachkiserver/WebKachkiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webkachkiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebKachkiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
