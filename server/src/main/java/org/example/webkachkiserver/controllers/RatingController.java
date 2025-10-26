46a5f49debb4e10c1237203207e7d0441fc338ca
guzzzlikz <jaroslavvolk9@gmail.com>
2025-10-26 20:59:36 +0200

    data type fix

--- server/src/main/java/org/example/webkachkiserver/controllers/RatingController.java
+++ server/src/main/java/org/example/webkachkiserver/controllers/RatingController.java
@@ -12,8 +12,8 @@ public class RatingController {
     @Autowired
     private CourseRepository courseRepository;
     @PostMapping("/{courseId}/rating")
-    public ResponseEntity<?> updateRating(@RequestParam int rate, @PathVariable long courseId) {
-        Course course = courseRepository.findById(courseId);
+    public ResponseEntity<?> updateRating(@RequestParam int rate, @PathVariable String courseId) {
+        Course course = courseRepository.findByCourseId(courseId);
         course.setRates(course.getRates() + 1);
         course.setRating(Math.round((
                 (course.getRating() + rate) / course.getRates()) * 10) / 10);
--- server/src/main/java/org/example/webkachkiserver/repositrories/CourseRepository.java
+++ server/src/main/java/org/example/webkachkiserver/repositrories/CourseRepository.java
@@ -4,5 +4,5 @@ import org.example.webkachkiserver.models.course.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CourseRepository extends MongoRepository<Course, String> {
-    Course findById(long id);
+    Course findByCourseId(String id);
 }
