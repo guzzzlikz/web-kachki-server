df95c2f94ae14637d3b125ff864d190ec48456b4
guzzzlikz <jaroslavvolk9@gmail.com>
2025-10-26 22:55:11 +0200

    apis changed

--- server/src/main/java/org/example/webkachkiserver/controllers/LessonController.java
+++ server/src/main/java/org/example/webkachkiserver/controllers/LessonController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/lessons")
+@RequestMapping("api/lessons")
 public class LessonController {
     @Autowired
     private LessonService lessonService;
--- server/src/main/java/org/example/webkachkiserver/controllers/VideoController.java
+++ server/src/main/java/org/example/webkachkiserver/controllers/VideoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("api/courses")
 public class VideoController {
 
     @Autowired
