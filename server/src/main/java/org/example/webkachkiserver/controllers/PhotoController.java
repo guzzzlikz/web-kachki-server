3c877ff081a37f4d53e766ecade2b139b897df88
Vlad Krylov <vladd7589@gmail.com>
2025-10-30 00:43:39 +0200

    PhotoStorageService

---
+++ server/src/main/java/org/example/webkachkiserver/controllers/PhotoController.java
@@ -0,0 +1,49 @@
+package org.example.webkachkiserver.controllers;
+
+import org.example.webkachkiserver.models.lesson.Lesson;
+import org.example.webkachkiserver.models.user.User;
+import org.example.webkachkiserver.repositrories.UserRepository;
+import org.example.webkachkiserver.services.PhotoStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("api/photo")
+public class PhotoController {
+
+    @Autowired
+    private PhotoStorageService photoStorageService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/{userId}/upload")
+    public ResponseEntity<?> uploadVideo(@PathVariable long userId,
+                                         @RequestParam("file") MultipartFile file) {
+        try {
+            String gcsPath = photoStorageService.uploadUserProfilePhoto(file, userId);
+            User user = userRepository.findById(userId);
+            user.setPathToPhoto(gcsPath);
+            if (!userRepository.existsById(userId)) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            userRepository.save(user);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Upload failed: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{userId}/user")
+    public User getUser(@PathVariable long userId) {
+        return photoStorageService.getUser(userId);
+    }
+    @GetMapping("/{userId}/photo")
+    public ResponseEntity<?> getUrl(@PathVariable long userId) {
+        return photoStorageService.getUrl(userId);
+    }
+}
---
+++ server/src/main/java/org/example/webkachkiserver/services/PhotoStorageService.java
@@ -0,0 +1,60 @@
+package org.example.webkachkiserver.services;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import org.checkerframework.checker.units.qual.A;
+import org.example.webkachkiserver.models.user.User;
+import org.example.webkachkiserver.repositrories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PhotoStorageService {
+
+    @Autowired
+    private Storage storage;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Value("${gcs.bucket.photo.name}")
+    private String bucketName;
+
+    public String uploadUserProfilePhoto(MultipartFile file, long userId) throws IOException {
+        String blobName = "photos/user/" + userId + "/" + file.getOriginalFilename();
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, blobName)
+                .setContentType(file.getContentType())
+                .build();
+        storage.create(blobInfo, file.getBytes());
+
+        return blobName;
+    }
+
+    public String getSignedUrl(String objectName) {
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, objectName).build();
+        URL signedUrl = storage.signUrl(
+                blobInfo,
+                15, TimeUnit.MINUTES,
+                Storage.SignUrlOption.withV4Signature()
+        );
+        return signedUrl.toString();
+    }
+
+    public User getUser(long userId) {
+        User user = userRepository.findById(userId);
+        String signedUrl = getSignedUrl(user.getPathToPhoto());
+        user.setPathToPhoto(signedUrl);
+        return user;
+    }
+
+    public ResponseEntity<?> getUrl(long lessonId) {
+        return ResponseEntity.ok(userRepository.findById(lessonId).getPathToPhoto());
+    }
+}
