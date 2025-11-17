e14bce734534cc442801a7f48b67ecb0f8b5d586
Vlad Krylov <vladd7589@gmail.com>
2025-11-18 00:54:27 +0200

    Add new url for cors

--- server/src/main/java/org/example/webkachkiserver/configs/CorsConfig.java
+++ server/src/main/java/org/example/webkachkiserver/configs/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "https://web-kachki-server-latest.onrender.com",
                         "https://test-server-kachki.vercel.app",
+                        "https://web-kachki-client2.vercel.app",
                         "https://reqbin.com/post-online");
     }
 }
